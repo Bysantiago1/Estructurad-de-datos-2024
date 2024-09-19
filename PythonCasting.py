360020e23fe529e777112a8e3d74a4df69504885
BySantiago <151958378+Bysantiago1@users.noreply.github.com>
2024-09-19 15:52:15 -0500

    Add files via upload

---
+++ PythonCasting.py
@@ -0,0 +1,16 @@
+# Al usar un tipo de dato cambiara a la forma de escritura 
+#Al usar int()
+x = int(10)   # sera 10
+y = int(3.77) # sera 4
+z = int("2") # sera 2 
+
+#Al usar float()
+x = float(100)     # sera 100.0
+y = float(3.77)   # sera 3.77
+z = float("2")   # sera 2.0
+w = float("2.2") # sera 2.2
+
+#Al usar srt()
+x = str("s10") # sera 's10'
+y = str(4)    # sera'4'
+z = str(2.0)  # sera '2.0'
